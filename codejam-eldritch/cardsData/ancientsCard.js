32f295bfa941a1f4e4416e61eae2c803901a41e8
mracoon <nataliyakomarova777@gmail.com>
2022-08-25 05:25:17 +0300

    feat: update layout and logic

---
+++ codejam-eldritch/cardsData/ancientsCard.js
@@ -0,0 +1,112 @@
+const ancientsData = {
+  azathoth: {
+    id: 'azathoth',
+    name: 'azathoth',
+    //cardFace: Ancients.azathoth,
+    firstStage: {
+      greenCards: 1,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 2,
+      blueCards: 1,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+    all: {
+      greenCards: 5,
+      blueCards: 2,
+      brownCards: 9,
+      total: 16
+    }
+  },
+
+  cthulhu:
+  {
+    id: 'cthulhu',
+    name: 'cthulhu',
+    //cardFace: Ancients.cthulhu,
+    firstStage: {
+      greenCards: 0,
+      blueCards: 2,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 1,
+      blueCards: 0,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 3,
+      blueCards: 0,
+      brownCards: 4,
+    },
+    all: {
+      greenCards: 4,
+      blueCards: 2,
+      brownCards: 9,
+      total: 15
+    }
+  },
+  iogSothoth:
+  {
+    id: 'iogSothoth',
+    name: 'iogSothoth',
+    //  cardFace: Ancients.iogSothoth,
+    firstStage: {
+      greenCards: 0,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 2,
+      blueCards: 1,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 3,
+      blueCards: 0,
+      brownCards: 4,
+    },
+    all: {
+      greenCards: 5,
+      blueCards: 2,
+      brownCards: 9,
+      total: 16
+    }
+  },
+  shubNiggurath:
+  {
+    id: 'shubNiggurath',
+    name: 'shubNiggurath',
+    //  cardFace: Ancients.shubNiggurath,
+    firstStage: {
+      greenCards: 1,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 3,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+    all: {
+      greenCards: 6,
+      blueCards: 2,
+      brownCards: 8,
+      total: 16
+    }
+  },
+}
+
+export default ancientsData
---
+++ codejam-eldritch/cardsData/blue.js
@@ -0,0 +1,87 @@
+const blueCardsData = {
+  easy: [
+    {
+      id: 'blue3',
+      path: '../assets/MythicCards/blue/blue3.png',
+      difficulty: 'easy',
+      color: 'blue'
+    },
+    {
+      id: 'blue4',
+      path: '../assets/MythicCards/blue/blue4.png',
+      difficulty: 'easy',
+      color: 'blue'
+    },
+    {
+      id: 'blue5',
+      path: '../assets/MythicCards/blue/blue5.png',
+      difficulty: 'easy',
+      color: 'blue'
+    },
+    {
+      id: 'blue10',
+      path: '../assets/MythicCards/blue/blue10.png',
+      difficulty: 'easy',
+      color: 'blue'
+    },
+
+  ],
+  normal: [
+
+    {
+      id: 'blue7',
+      path: '../assets/MythicCards/blue/blue7.png',
+      difficulty: 'normal',
+      color: 'blue'
+    },
+    {
+      id: 'blue9',
+      path: '../assets/MythicCards/blue/blue9.png',
+      difficulty: 'normal',
+      color: 'blue'
+    },
+
+    {
+      id: 'blue11',
+      path: '../assets/MythicCards/blue/blue11.png',
+      difficulty: 'normal',
+      color: 'blue'
+    },
+    {
+      id: 'blue12',
+      path: '../assets/MythicCards/blue/blue12.png',
+      difficulty: 'normal',
+      color: 'blue'
+    },
+
+  ],
+  hard: [
+    {
+      id: 'blue1',
+      path: '../assets/MythicCards/blue/blue1.png',
+      difficulty: 'hard',
+      color: 'blue'
+    },
+    {
+      id: 'blue2',
+      path: '../assets/MythicCards/blue/blue2.png',
+      difficulty: 'hard',
+      color: 'blue'
+    },
+    {
+      id: 'blue8',
+      path: '../assets/MythicCards/blue/blue8.png',
+      difficulty: 'hard',
+      color: 'blue'
+    },
+    {
+      id: 'blue6',
+      path: '../assets/MythicCards/blue/blue6.png',
+      difficulty: 'hard',
+      color: 'blue'
+    },
+  ]
+
+}
+
+export default blueCardsData;
